badb546dded7e99a4b72930fab74dfe2f7f7b9f4
Aline dos Santos Cardoso <alinecardoso@brq.com>
2025-11-23 22:34:05 -0300

    Add rodape

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent],
+  imports: [
+    CabecalhoComponent, 
+    RodapeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
